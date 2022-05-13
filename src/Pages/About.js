f1764454337bb09f4e3b29fed34f129a4790d06a
Michael Cheung <michaelcheung.cheme@gmail.com>
2022-05-13 02:12:15 -0600

    changed web title and added to about

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return(
     <div>
       <h1> About Page</h1>
-      <p></p>
+      <p> Went to the University of Utah and got a B.S. in Chemical Engineering. Went to DevPoint Labs to learn full stack web development in Fall of 2021. Current languages known is JavaScript and Ruby. Web dev stack is currently React on Rails.</p>
     </div>
   )
 };
